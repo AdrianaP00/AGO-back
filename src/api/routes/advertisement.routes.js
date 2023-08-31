931043c657c73ec9e0cfec49b10165cd989547cd
Adriana Piccolo <adrianapiccolo00@gmail.com>
2023-08-31 16:01:30 +0200

    fix

--- src/api/routes/advertisement.routes.js
+++ src/api/routes/advertisement.routes.js
@@ -9,6 +9,6 @@ advRoutes.get("/",[isAuth],[isAdmin],[isUser],[isCompany],getAdvertisement);
 advRoutes.get("/:id",[isAuth],[isAdmin],[isUser],[isCompany], getOneAdvertisement)
 advRoutes.post("/",[isAuth],[isAdmin],[isCompany], postAdvertisement);
 advRoutes.put("/:id",[isAuth],[isAdmin],[isCompany], putAdvertisement);
-advRoutes.delete("/:id"[isAuth],[isAdmin],[isCompany],deleteAdvertisement);
+advRoutes.delete("/:id",[isAuth],[isAdmin],[isCompany],deleteAdvertisement);
 
 module.exports= advRoutes;
