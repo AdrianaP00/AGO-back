b2f4210225960c8dfd0676eb246ab785c8921b21
Adriana Piccolo <adrianapiccolo00@gmail.com>
2023-09-05 04:29:44 +0200

    swagger

--- src/api/routes/advertisement.routes.js
+++ src/api/routes/advertisement.routes.js
@@ -15,10 +15,209 @@ const {
 
 const advRoutes = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     advertisement:
+ *       type: object
+ *       properties:
+ *         title:
+ *           type: string
+ *           description: advertisement title
+ *         description:
+ *           type: string
+ *           description: description of the advertisement
+ *         salary:
+ *           type: string
+ *           description: salary of the advertisement
+ *         extra:
+ *           type: string
+ *           description: advertisement extras
+ *         localization:
+ *           type: string
+ *           description: advertisement localization
+ *         skills:
+ *            type: string
+ *            description: advertisement skills required
+ *         img:
+ *            type: string
+ *            description: Number of employes
+ *         map:
+ *            type: object
+ *            items:
+ *              type: string
+ *            uniqueItems: true
+ *            description: string location on map
+ *         users:
+ *            type: array
+ *            items:
+ *              type: string
+ *            uniqueItems: true
+ *            description: advertisement user
+ *         jobs:
+ *           type: array
+ *           items:
+ *             type: string
+ *           uniqueItems: true
+ *           description: advertisement job
+ *       required:
+ *         - title
+ *         - description
+ *         - salary
+ *         - extra
+ *         - localization
+ *         - skills
+ *         - users
+ *         - jobs
+ *         - img
+ *         - map
+ *       example:
+ *         title: Desarrollador Full Stack
+ *         company: TechCo
+ *         description: Buscamos un desarrollador Full Stack para trabajar en proyectos emocionantes
+ *         salary: Negociable
+ *         extra: Beneficios adicionales incluidos
+ *         localization: Ciudad A
+ *         skills: JavaScript, React, Node.js, MongoDB
+ *         map: 64f332890873d39086ca8172
+ */
+
+
+/**
+ * @swagger
+ * /advertisement:
+ *   get:
+ *     summary: Get all advertisements
+ *     tags: [Advertisement]
+ *     responses:
+ *       200:
+ *         description: All advertisements
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Advertisement'
+ *       500:
+ *         description: Internal server error
+ */
 advRoutes.get("/", getAdvertisements);
+
+/**
+ * @swagger
+ * /advertisement/{id}:
+ *   get:
+ *     summary: Obtain information about a specific advertisement by ID
+ *     tags: [Advertisement]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Advertisement ID
+ *     responses:
+ *       200:
+ *         description: Obtain information about a specific Advertisement by ID
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               $ref: '#/components/schemas/Advertisement'
+ *       500:
+ *         description: Internal server error
+ */
 advRoutes.get("/:id", getAdvertisement);
+
+/**
+ * @swagger
+ * /advertisement:
+ *   post:
+ *     summary: Create a new advertisement
+ *     tags: [Advertisement]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             $ref: '#/components/schemas/Advertisement'
+ *     responses:
+ *       200:
+ *         description: Created Advertisement
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               $ref: '#/components/schemas/Advertisement'
+ *       500:
+ *         description: Internal server error
+ */
 advRoutes.post("/", [isAuth], postAdvertisement);
+
+/**
+ * @swagger
+ * /advertisement/{id}:
+ *   put:
+ *     summary: Modify a specific Advertisement by ID
+ *     tags: [Advertisement]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             $ref: '#/components/schemas/Advertisement'
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Advertisement ID
+ *     responses:
+ *       200:
+ *         description: Modified Advertisement
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               $ref: '#/components/schemas/Advertisement'
+ *       404:
+ *         description: Advertisement not found
+ *       500:
+ *         description: Internal server error
+ */
 advRoutes.put("/:id", [isAuth], putAdvertisement);
+
+/**
+ * @swagger
+ * /advertisement/{id}:
+ *   delete:
+ *     summary: Delete a specific Advertisement by ID
+ *     tags: [Advertisement]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Advertisement ID
+ *     responses:
+ *       200:
+ *         description: Advertisement deleted
+ *       404:
+ *         description: Advertisement not found
+ *       500:
+ *         description: Internal server error
+ */
 advRoutes.delete("/:id", [isAuth], deleteAdvertisement);
 
 module.exports = advRoutes;
--- src/api/routes/comment.routes.js
+++ src/api/routes/comment.routes.js
@@ -15,10 +15,187 @@ const {
 
 const commentRouter = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Comments:
+ *       type: object
+ *       properties:
+ *         text:
+ *           type: string
+ *           description: text of comment
+ *         score:
+ *            type: number
+ *            description: rate
+ *         jobs:
+ *            type: array
+ *            items:
+ *              type: string
+ *            uniqueItems: true
+ *            description: job about comment
+ *         company:
+ *            type: array
+ *            items:
+ *              type: string
+ *            uniqueItems: true
+ *            description: company
+ *         user:
+ *           type: array
+ *           items:
+ *             type: string
+ *           uniqueItems: true
+ *           description: user
+ *       required:
+ *         - text
+ *         - user
+ *         - company
+ *         - score
+ *         - jobs
+ *         - img
+ *       example:
+ *         text: ¡Excelente trabajo! Realmente impresionado con los resultados
+ *         user: 64f1f76422539ea74d29fa28
+ *         company: []
+ *         score: 5
+ *         jobs: []
+ *         img: url_de_la_imagen1
+ */
+
+/**
+ * @swagger
+ * /comments:
+ *   get:
+ *     summary: Get all comments
+ *     tags: [Comments]
+ *     responses:
+ *       200:
+ *         description: All comments
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Comments'
+ *       500:
+ *         description: Internal server error
+ */
 commentRouter.get("/", [isAuth], getComments);
+
+/**
+ * @swagger
+ * /comments/{id}:
+ *   get:
+ *     summary: Obtain information about a specific comments by ID
+ *     tags: [Comments]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Comments ID
+ *     responses:
+ *       200:
+ *         description: Obtain information about a specific Comments by ID
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               $ref: '#/components/schemas/Comments'
+ *       500:
+ *         description: Internal server error
+ */
 commentRouter.get("/:id", [isAuth], getComment);
+
+/**
+ * @swagger
+ * /comments/:
+ *   post:
+ *     summary: Create a new job
+ *     tags: [Comments]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             $ref: '#/components/schemas/Comments'
+ *     responses:
+ *       200:
+ *         description: Created Comments
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               $ref: '#/components/schemas/Comments'
+ *       500:
+ *         description: Internal server error
+ */
 commentRouter.post("/", [isAuth], postComment);
+
+/**
+ * @swagger
+ * /comments/{id}:
+ *   put:
+ *     summary: Modify a specific comments by ID
+ *     tags: [Comments]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             $ref: '#/components/schemas/Comments'
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Comments ID
+ *     responses:
+ *       200:
+ *         description: Modified Comments
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               $ref: '#/components/schemas/Comments'
+ *       404:
+ *         description: Comments not found
+ *       500:
+ *         description: Internal server error
+ */
 commentRouter.put("/:id", [isAuth], putComment);
+
+/**
+ * @swagger
+ * /comments/{id}:
+ *   delete:
+ *     summary: Delete a specific Comments by ID
+ *     tags: [Comments]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Comments ID
+ *     responses:
+ *       200:
+ *         description: Comments deleted
+ *       404:
+ *         description: Comments not found
+ *       500:
+ *         description: Internal server error
+ */
 commentRouter.delete("/:id", [isAdmin], deleteComment);
 
 module.exports = commentRouter;
